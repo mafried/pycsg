1487e63879542cb98e0ac05f53e5f4ab7e420ea8
Markus Friedrich <markus.friedrich@ifi.lmu.de>
2020-04-20 17:49:28 +0200

    added cylinder primitive

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pycsg.primitives import Sphere, Box
 from pycsg.transforms import Pose
 from pycsg.sampling import point_cloud_from_node
 
-tree = Union('u2', [Sphere(1.0,'s1'), Pose([1.0, 0.0, 0.0], [45.0,45.0,45.0], [Box([1.0,1.0,1.0],'b1')], 'p1')])
+tree = Union('u2', [Cylinder(1.0,2.0,'s1'), Pose([1.0, 0.0, 0.0], [45.0,45.0,45.0], [Box([1.0,1.0,1.0],'b1')], 'p1')])
 
 print(tree.to_dict())
 
--- pycsg/primitives.py
+++ pycsg/primitives.py
@@ -49,5 +49,39 @@ class Box(CSGNode):
         return 'box'
 
 
+class Cylinder(CSGNode):
+    def __init__(self, radius, height, name):
+        super().__init__(name)
+
+        self.radius = radius
+        self.height = height
+
+    def signed_distance(self, p):
+
+        return np.max(np.stack(
+                (
+                    (np.linalg.norm(p[:, [0, 2]], axis=1) - self.radius),
+                    (np.abs(p[:, 1]) - self.height / 2.0)
+                ),
+                axis=1
+            ), axis=1
+        )
+
+    def to_dict(self):
+        d = super().to_dict().copy()
+        d['radius'] = self.radius
+        d['height'] = self.height
+        return d
+
+    @staticmethod
+    def from_dict(d, children):
+        return Cylinder(d['radius'], d['height'],d['name'])
+
+    @staticmethod
+    def node_type():
+        return 'cylinder'
+
+
 register_node_type(Sphere)
 register_node_type(Box)
+register_node_type(Cylinder)
